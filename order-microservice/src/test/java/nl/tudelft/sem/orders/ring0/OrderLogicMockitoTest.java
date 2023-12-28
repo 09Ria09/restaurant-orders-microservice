c463fa642afb8bc4ac031af843ebd4df0280e484
Jakub Rutkowski <kuba.r0012@gmail.com>
2023-12-27 19:18:26 -0500

    add reorder order endpoint tests

--- order-microservice/src/test/java/nl/tudelft/sem/orders/ring0/OrderLogicMockitoTest.java
+++ order-microservice/src/test/java/nl/tudelft/sem/orders/ring0/OrderLogicMockitoTest.java
@@ -3,6 +3,8 @@ package nl.tudelft.sem.orders.ring0;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -15,10 +17,14 @@ import javax.validation.Valid;
 import nl.tudelft.sem.orders.model.Dish;
 import nl.tudelft.sem.orders.model.Location;
 import nl.tudelft.sem.orders.model.Order;
+import nl.tudelft.sem.orders.model.OrderDishesInner;
 import nl.tudelft.sem.orders.model.OrderOrderIDDishesPutRequestDishesInner;
 import nl.tudelft.sem.orders.ports.output.DishDatabase;
+import nl.tudelft.sem.orders.ports.output.LocationService;
 import nl.tudelft.sem.orders.ports.output.OrderDatabase;
 import nl.tudelft.sem.orders.ports.output.UserMicroservice;
+import nl.tudelft.sem.orders.result.MalformedException;
+import nl.tudelft.sem.orders.result.NotFoundException;
 import nl.tudelft.sem.orders.test.mocks.MockPaymentService;
 import nl.tudelft.sem.users.ApiException;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,14 +36,22 @@ public class OrderLogicMockitoTest {
     DishDatabase dishDatabase;
     private UserMicroservice userMicroservice;
     private OrderLogic orderFacade;
+    private LocationService locationService;
 
     @BeforeEach
     void setUp() {
         orderDatabase = mock(OrderDatabase.class);
         dishDatabase = mock(DishDatabase.class);
         userMicroservice = mock(UserMicroservice.class);
+        locationService = mock(LocationService.class);
 
-        orderFacade = new OrderLogic(orderDatabase, dishDatabase, userMicroservice, new MockPaymentService());
+        orderFacade = new OrderLogic(
+            orderDatabase,
+            dishDatabase,
+            userMicroservice,
+            new MockPaymentService(),
+            locationService
+        );
     }
 
     @Test
@@ -143,5 +157,90 @@ public class OrderLogicMockitoTest {
         when(orderDatabase.getById(orderId)).thenReturn(order);
 
         assertThrows(IllegalStateException.class, () -> orderFacade.updateDishes(orderId, customerId, dishes));
+    }
+
+    @Test
+    public void testReorder() throws Exception {
+        Dish dish = new Dish(1L,
+            2L,
+            "name",
+            "description",
+            new ArrayList<>(),
+            1.0f);
+        OrderDishesInner dishInner = new OrderDishesInner(dish, 1);
+        Order order = new Order(1L,
+            1L,
+            2L,
+            new ArrayList<>(List.of(dishInner)),
+            null,
+            Order.StatusEnum.ACCEPTED);
+
+        when(orderDatabase.getById(1L)).thenReturn(order);
+        when(userMicroservice.isVendor(2L)).thenReturn(true);
+        when(locationService.isCloseBy(any(), any())).thenReturn(true);
+        when(dishDatabase.getById(1L)).thenReturn(dish);
+
+        Order newOrder = orderFacade.reorder(1L, 1L);
+        assertEquals(order.getCustomerID(), newOrder.getCustomerID());
+        assertEquals(order.getVendorID(), newOrder.getVendorID());
+        assertEquals(Order.StatusEnum.UNPAID, newOrder.getStatus());
+    }
+
+    @Test
+    public void testReorderMalformedExceptionOrderNotExisting() {
+        when(orderDatabase.getById(anyLong())).thenReturn(null);
+
+        assertThrows(MalformedException.class, () -> orderFacade.reorder(1L, 1L));
+    }
+
+    @Test
+    public void testReorderMalformedExceptionOrderNotOwned() {
+        Order order = new Order(1L, 1L, 1L, null, null, Order.StatusEnum.ACCEPTED);
+        when(orderDatabase.getById(1L)).thenReturn(order);
+
+        assertThrows(MalformedException.class, () -> orderFacade.reorder(2L, 1L));
+    }
+
+    @Test
+    public void testReorderNotFoundExceptionVendorNotExisting() throws Exception {
+        Order order = new Order(1L, 1L, 1L, null, null, Order.StatusEnum.UNPAID);
+        when(orderDatabase.getById(1L)).thenReturn(order);
+        when(userMicroservice.isVendor(1L)).thenReturn(false);
+
+        assertThrows(NotFoundException.class, () -> orderFacade.reorder(1L, 1L));
+    }
+
+    @Test
+    public void testReorderNotFoundExceptionVendorNotCloseBy() throws Exception {
+        Order order = new Order(1L, 1L, 1L, null, null, Order.StatusEnum.UNPAID);
+        when(orderDatabase.getById(1L)).thenReturn(order);
+        when(userMicroservice.isVendor(1L)).thenReturn(true);
+        when(locationService.isCloseBy(any(), any())).thenReturn(false);
+
+        assertThrows(NotFoundException.class, () -> orderFacade.reorder(1L, 1L));
+    }
+
+    @Test
+    public void testReorderNotFoundExceptionDishNotExisting() throws Exception {
+        Dish dish = new Dish(1L,
+            2L,
+            "name",
+            "description",
+            new ArrayList<>(),
+            1.0f);
+        OrderDishesInner dishInner = new OrderDishesInner(dish, 1);
+        Order order = new Order(1L,
+            1L,
+            2L,
+            new ArrayList<>(List.of(dishInner)),
+            null,
+            Order.StatusEnum.ACCEPTED);
+
+        when(orderDatabase.getById(1L)).thenReturn(order);
+        when(userMicroservice.isVendor(2L)).thenReturn(true);
+        when(locationService.isCloseBy(any(), any())).thenReturn(true);
+        when(dishDatabase.getById(1L)).thenReturn(null);
+
+        assertThrows(NotFoundException.class, () -> orderFacade.reorder(1L, 1L));
     }
 }
